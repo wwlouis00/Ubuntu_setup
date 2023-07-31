0f3fe1783547d29c4cdc109b0a96bbf276ab08e6
wwlouis00 <wwlouis00@gmail.com>
2023-08-01 04:10:22 +0800

    Create zsh_setup.sh

---
+++ zsh_setup.sh
@@ -0,0 +1,16 @@
+
+
+
+sudo apt update 
+sudo apt install zsh git curl vim
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+
